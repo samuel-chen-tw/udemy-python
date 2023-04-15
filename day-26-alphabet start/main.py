e3720445776a1b0b35900672fe5dcaf5e3248768
Samuel <samuel811215@gmail.com>
2023-04-15 15:37:19 +0800

    Day-27-Alphabet
    use panda framework to get the csv file data, and create dictionary format

---
+++ day-26-alphabet start/main.py
@@ -0,0 +1,34 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+df = pandas.DataFrame(pandas.read_csv("nato_phonetic_alphabet.csv"))
+new_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+print(new_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+input_name = input("Your name: ").upper()
+print(input_name)
+# result = [code for (letter, code) in new_dict.items() if letter in input_name]
+result = [new_dict[letter] for letter in input_name]
+print(result)
+
